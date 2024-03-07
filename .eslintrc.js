0b2a9f724154177113025f571e33f4c6a05acb4e
Hossam Gamal <68892879+Hossam404@users.noreply.github.com>
2024-03-08 01:07:15 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   extends: 'eslint:recommended',
-  env: {
-    node: true,
-  },
   rules: {
       'indent': ['error', 2],
       'semi': ['error', 'never'],
       'quotes': ['error', 'single'],
       'comma-dangle': ['error', 'always-multiline'],
       'keyword-spacing': ['error', { 'before': true, 'after': true }],
-      
   },
 };
